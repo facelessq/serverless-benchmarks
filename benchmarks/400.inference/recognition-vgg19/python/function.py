491360c3bd470f9854dfb961de2cd92ce5bba4b0
QW <facelessq@icloud.com>
2022-01-13 13:51:03 +0100

    add usm to vgg19

--- benchmarks/400.inference/recognition-vgg19/python/function.py
+++ benchmarks/400.inference/recognition-vgg19/python/function.py
@@ -6,6 +6,9 @@ import torch
 from torchvision import transforms
 from torchvision.models import resnet50
 
+from _mman_cffi import ffi,lib
+import sys
+
 from . import storage
 client = storage.storage.get_instance()
 
@@ -13,10 +16,10 @@ SCRIPT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__)))
 class_idx = json.load(open(os.path.join(SCRIPT_DIR, "imagenet_class_index.json"), 'r'))
 idx2label = [class_idx[str(k)][1] for k in range(len(class_idx))]
 model = None
-
+pagesize = 4096
 
 def handler(event):
-  
+    handler_begin = datetime.datetime.now()
     model_bucket = event.get('bucket').get('model')
     input_bucket = event.get('bucket').get('input')
     key = event.get('object').get('input')
@@ -43,7 +46,71 @@ def handler(event):
         model_download_end = model_download_begin
         model_process_begin = datetime.datetime.now()
         model_process_end = model_process_begin
-   
+    
+    def test(model):
+        for key, param in model._modules.items():
+            if param is not None:
+                if hasattr(param, 'weight'):
+                    if param.weight is not None:
+                        addr = param.weight.data_ptr()
+                        addr_rounded = param.weight.data_ptr() & ~(pagesize - 1)
+                        size = param.weight.size().numel() * 4
+                        # size_total += size
+                        size_round_up = (size + pagesize - 1) & ~(pagesize - 1)
+                        # print("addr", hex(addr), "addr_rounded", hex(addr_rounded), key, ".", "weight", param.weight.size(), size, "/", size_round_up, size_round_up/4096, "page(s)")
+                        # madvise_page += size_round_up/4096
+                        ret = lib.usm_madvise(addr_rounded, size_round_up)
+                        if ret < 0:
+                            print("madvise ret < 0")
+                if hasattr(param, 'bias'):
+                    if param.bias is not None:
+                        addr = param.bias.data_ptr() & ~(pagesize - 1)
+                        size = param.bias.size().numel() * 4
+                        # size_total += size
+                        size_round_up = (size + pagesize - 1) & ~(pagesize - 1)
+                        # print(key, ".", "bias", param.bias.size(), size, "/", size_round_up, size_round_up/4096, "page(s)")
+                        # madvise_page += size_round_up/4096
+                        ret = lib.usm_madvise(addr, size_round_up)
+                        if ret < 0:
+                            print("madvise ret < 0")
+                if hasattr(param, 'running_mean'):
+                    if param.running_mean is not None:
+                        addr = param.running_mean.data_ptr() & ~(pagesize - 1)
+                        size = param.running_mean.size().numel() * 4
+                        # size_total += size
+                        size_round_up = (size + pagesize - 1) & ~(pagesize - 1)
+                        # print(key, ".", "running_mean", param.running_mean.size(), size, "/",  size_round_up, size_round_up/4096, "page(s)")
+                        # madvise_page += size_round_up/4096
+                        ret = lib.usm_madvise(addr, size_round_up)
+                        if ret < 0:
+                            print("madvise ret < 0")
+                if hasattr(param, 'running_var'):
+                    if param.running_var is not None:
+                        addr = param.running_var.data_ptr() & ~(pagesize - 1)
+                        size = param.running_var.size().numel() * 4
+                        # size_total += size
+                        size_round_up = (size + pagesize - 1) & ~(pagesize - 1)
+                        # print(key, ".", "running_var", param.running_var.size(), size, "/", size_round_up, size_round_up/4096, "page(s)")
+                        # madvise_page += size_round_up/4096
+                        ret = lib.usm_madvise(addr, size_round_up)
+                        if ret < 0:
+                            print("madvise ret < 0")
+                if hasattr(param, 'num_batches_tracked'):
+                    if param.num_batches_tracked is not None:
+                        addr = param.num_batches_tracked.data_ptr() & ~(pagesize - 1)
+                        size = param.num_batches_tracked.size().numel() * 4
+                        # size_total += size
+                        size_round_up = (size + pagesize - 1) & ~(pagesize - 1)
+                        # print(key, ".", "num_batches_tracked", param.num_batches_tracked.size(), size, "/", size_round_up, size_round_up/4096, "page(s)")
+                        # madvise_page += size_round_up/4096
+                        ret = lib.usm_madvise(addr, size_round_up)
+                        if ret < 0:
+                            print("madvise ret < 0")
+                test(param)
+    
+    madvise_begin = datetime.datetime.now()
+    test(model)
+    madvise_end = datetime.datetime.now()
     process_begin = datetime.datetime.now()
     input_image = Image.open(image_path)
     preprocess = transforms.Compose([
@@ -66,13 +133,17 @@ def handler(event):
     model_download_time = (model_download_end - model_download_begin) / datetime.timedelta(microseconds=1)
     model_process_time = (model_process_end - model_process_begin) / datetime.timedelta(microseconds=1)
     process_time = (process_end - process_begin) / datetime.timedelta(microseconds=1)
+    madvise_time = (madvise_end - madvise_begin) / datetime.timedelta(microseconds=1)
+    handler_time = (process_end - handler_begin) / datetime.timedelta(microseconds=1)
     return {
             'result': {'idx': index.item(), 'class': ret},
             'measurement': {
                 'download_time': download_time + model_download_time,
                 'compute_time': process_time + model_process_time,
                 'model_time': model_process_time,
-                'model_download_time': model_download_time
+                'model_download_time': model_download_time,
+                'madvise_time': madvise_time,
+                'handler_time': handler_time
             }
         }
 
